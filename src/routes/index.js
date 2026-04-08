954d0ad94205ae3e56983399e3946462cc576179
kerliix-corp <dev@kerliix.com>
2026-04-08 22:10:17 +0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,12 @@ import { Router } from "express";
 import pagesRouter from "./pages.js";
 import apiRouter from "./apiRoutes.js";
 import orderRouter from "./orderRoutes.js";
-import legaRouter from "./legalRoutes.js";
 
 const router = Router();
 
 router.use("/", pagesRouter);
 router.use("/api", apiRouter);
 router.use("/api", orderRouter);
-router.use("/", legalRouter);
 
 export default router;
 
