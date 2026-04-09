80991943c26c2f376ed7488061f5e02b7d9c4876
kerliix-corp <dev@kerliix.com>
2026-04-09 13:30:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 import "dotenv/config";
+import http from "http";
 
 import app from "./src/app.js";
+import connectDB from "./config/db.js";
+import logger from "./config/logger.js";
+import initSocket from "./config/socket.js";
 
 const port = Number(process.env.PORT || 3000);
 
-app.listen(port, () => {
-  console.log(`Merchant app running at http://localhost:${port}`);
+const server = http.createServer(app);
+
+initSocket(server);
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    server.listen(port, () => {
+      logger.info(`Merchant app running at http://localhost:${port}`);
+    });
+  } catch (error) {
+    logger.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+process.on("unhandledRejection", (err) => {
+  logger.error("Unhandled Rejection:", err);
+  process.exit(1);
 });
+
+process.on("uncaughtException", (err) => {
+  logger.error("Uncaught Exception:", err);
+  process.exit(1);
+});
+
+
